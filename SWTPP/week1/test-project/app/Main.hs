832b01821188df6edbadc3072183b0c80f15314a
comdobby <tofk1107@gmail.com>
2025-10-26 19:17:58 +0100

    add print for testing calculator functions

--- SWTPP/week1/test-project/app/Main.hs
+++ SWTPP/week1/test-project/app/Main.hs
@@ -3,4 +3,11 @@ module Main (main) where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    print(fee)
+    print(charge 0)
+    print(charge 2)
+    print(putChips 10 2)
+    print(takeChips 10 11)
+    print(takeChips 10 8)
+    print(win 20 5)
