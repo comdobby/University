d734db3e375d1bca8c3160dcc7cb1a443bae086f
comdobby <tofk1107@gmail.com>
2025-10-27 22:54:18 +0100

    add sum type 'CommandS' with execute function 'eval'

---
+++ SWTPP/week2/Aufgabe2.hs
@@ -0,0 +1,36 @@
+-- Verwendet deriving Show
+-- deriving Show konveriert Datentyp in String
+
+-- a) implementieren Aufzählungstyp CommandS, CommandS hat Put, Take und Win
+
+-- Hilfsfunktionen
+fee :: Int
+fee = 1
+
+charge :: Int -> Int
+charge val = if val == 0 then 0 else val - fee
+
+putChips :: Int -> Int -> Int
+putChips owned added = charge (owned + added)
+
+takeChips :: Int -> Int -> Int
+takeChips owned taken = if owned < taken then 0 else owned - taken
+
+winTR :: Int -> Int -> Int -> Int
+winTR _ 0 acc = acc
+winTR a b acc
+  | a < b = winTR b a acc
+  | a `mod` 10 == 0 = winTR (a - 1) (b - 1) $! (b + acc)
+  | otherwise = winTR (a - 1) b $! (b + acc)
+
+win :: Int -> Int -> Int
+win a b = winTR a b 0
+
+-- CommandS
+data CommandS = Put | Take | Win deriving Show
+
+-- eval
+eval :: CommandS -> Int -> Int -> Int
+eval Put = putChips
+eval Take = takeChips
+eval Win = win
