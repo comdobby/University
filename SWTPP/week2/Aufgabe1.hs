564a971e131a75413ca0383ebf9bd4fa715c71b0
comdobby <tofk1107@gmail.com>
2025-10-27 22:26:00 +0100

    optimize win function

--- SWTPP/week2/Aufgabe1.hs
+++ SWTPP/week2/Aufgabe1.hs
@@ -6,4 +6,18 @@ win a b
   | a `mod` 10 == 0 = b + win (a - 1) (b - 1)
   | otherwise = b + win (a - 1) b
 
--- b)
+
+-- b) optimieren win-Funktion mit weniger Speicher
+-- Lazy Evaluation: Haskell postpones calculations until it nees -> it uses lots of memory
+-- Use tail recursion + strict evaluation
+winTR :: Int -> Int -> Int -> Int
+winTR _ 0 acc = acc
+winTR a b acc
+  | a < b = winTR b a acc
+  | a `mod` 10 == 0 = winTR (a - 1) (b - 1) $! (b + acc)
+  | otherwise = winTR (a - 1) b (b + acc)
+
+win2 :: Int -> Int -> Int
+win2 a b = winTR a b 0
+
+-- c) implementieren fib-Funktion und optimieren die Funktion
